43d718edca0519c42e8af6d06314459091de0c87
Ivan Daniel <84136621+doctoxer@users.noreply.github.com>
2024-07-11 14:38:44 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -8,7 +8,12 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @MD_Bots\n○ Support Group : @MD_Support_Group \n○ Credits : @CodeXBotz</b>",
+            text = f"""<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>
+            ○ Language : <code>Python3</code>
+            ○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>
+            ○ Channel : @TeamMoviePalace
+            ○ Support Group : @MoviePalaceSupport
+            ○ Credits : @ZenyxZone</b>""",
             disable_web_page_preview = False,
             reply_markup = InlineKeyboardMarkup(
                 [
